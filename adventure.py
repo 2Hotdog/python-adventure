58711cda4e03741f79d0b3dca9d98ba2c90d02af
2Hotdog <zahii@atomicmail.io>
2025-12-14 12:28:24 -0500

    help

---
+++ adventure.py
@@ -0,0 +1,11 @@
+Coins = 1000
+Coins_Stole = 100
+Fee = 200
+Coins = Coins + Coins_Stole - Fee
+
+potion = "Green"
+if potion == "Green":
+    print(f"You Had {Coins} Coins And You Got Poisoned . Game Over.")
+    
+Person_Poisoned_You = "Shopkeeper"
+print(f"This Is The Person Who Poisoned You Is The: {Person_Poisoned_You}.")
